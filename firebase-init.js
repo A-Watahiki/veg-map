fe738e7513181d735f3ef35b51c5599f9d246960
Amane Watahiki <amane@MacBook-Air-8.local>
2025-04-28 11:15:32 +0900

    バグ修正

--- firebase-init.js
+++ firebase-init.js
@@ -1,3 +1,4 @@
+```javascript
 // firebase-init.js
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js';
@@ -5,6 +6,7 @@ import {
   getFunctions,
   httpsCallable
 } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-functions.js';
+import { initializeFirestore } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDa-5ULVWt44aHOKFui6y4bn-tKCJk4xQY",
@@ -18,20 +20,26 @@ const firebaseConfig = {
 
 // Firebase アプリ初期化
 const app = initializeApp(firebaseConfig);
+
 // Auth
 export const auth = getAuth(app);
+
+// Firestore（veg-map データベース）
+export const db = initializeFirestore(app, {}, 'veg-map');
+
 // Functions (asia-northeast1)
 const functions = getFunctions(app, 'asia-northeast1');
 
 // Cloud Function 呼び出しラッパー
-// ※ Cloud Function で Admin SDK を使い、veg-map データベースを参照
-export const verifyUsername = httpsCallable(functions, 'verifyUsername');
-export const searchPlacesFn  = httpsCallable(functions, 'searchPlaces');
+export const verifyUsername     = httpsCallable(functions, 'verifyUsername');
+export const searchPlacesFn     = httpsCallable(functions, 'searchPlaces');
 export const getVegetarianFlagFn = httpsCallable(functions, 'getVegetarianFlag');
 
 // Firebase初期化完了を通知するイベントを発火
 window.dispatchEvent(new Event('firebaseReady'));
 
 // デバッグ用グローバル公開
-window.auth     = auth;
+window.auth      = auth;
+window.db        = db;
 window.functions = functions;
+```
